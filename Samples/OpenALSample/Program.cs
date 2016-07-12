1d1eb2be5341e018004e62102e2c24405bdd26b8
Patrick Yates <frozen.dinosaur@gmail.com>
2016-07-12 11:27:57 +1000

    Use Codec Factory in sample application
    Also fixed some whitespace issues

--- Samples/OpenALSample/Program.cs
+++ Samples/OpenALSample/Program.cs
@@ -29,7 +29,7 @@ namespace OpenALSample
 			_soundOut = new ALSoundOut ();
 			_soundOut.Initialize (_waveSource);
 
-			_soundOut.Play ();
+			_soundOut.Play();
 
 			while (true)
 			{
@@ -40,20 +40,20 @@ namespace OpenALSample
 						break;
 				}
 
-				var str = String.Format (@"{0:mm\:ss\.f}/{1:mm\:ss\.f}",
+				var str = string.Format (@"{0:mm\:ss\.f}/{1:mm\:ss\.f}",
 					          TimeConverterFactory.Instance.GetTimeConverterForSource (_waveSource)
 					.ToTimeSpan (_waveSource.WaveFormat, _waveSource.Position),
 					          TimeConverterFactory.Instance.GetTimeConverterForSource (_waveSource)
 					.ToTimeSpan (_waveSource.WaveFormat, _waveSource.Length));
-				str += String.Concat (Enumerable.Repeat (" ", Console.BufferWidth - 1 - str.Length));
+				str += string.Concat (Enumerable.Repeat (" ", Console.BufferWidth - 1 - str.Length));
 				Console.SetCursorPosition (0, Console.CursorTop);
 				Console.Write (str);
 				Thread.Sleep (100);
 			}
-			_soundOut.Stop ();
-			_soundOut.Dispose ();
-			_waveSource.Dispose ();
-			CSCore.SoundOut.AL.ALDevice.DefaultDevice.Dispose ();
+			_soundOut.Stop();
+			_soundOut.Dispose();
+			_waveSource.Dispose();
+			CSCore.SoundOut.AL.ALDevice.DefaultDevice.Dispose();
 		}
 	}
 }
